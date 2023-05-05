c6824e0a276d622a89d575492a419de2b206c24f
Alister <alister7@gmail.com>
2023-05-05 23:23:21 +0900

    fix: cleanup main method

--- cli/main.go
+++ cli/main.go
@@ -6,24 +6,4 @@ import (
 
 func main() {
 	presentation.RootCmd.Execute()
-
-	// example fetching and printing arbitrary data
-
-	// client, err := application.GetDbClient()
-	// if err != nil {
-	// 	fmt.Println("Failed to create DynamoDB client")
-	// 	return
-	// }
-
-	// result, err := application.QueryTable(client, tableName, "type", "book")
-	// if err != nil {
-	// 	fmt.Println("Failed to query table")
-	// 	return
-	// }
-
-	// for _, item := range result.Items {
-	// 	for k, v := range item {
-	// 		fmt.Printf("Result was %v: %v \n", k, v)
-	// 	}
-	// }
 }
